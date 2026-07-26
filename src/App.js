b9e18b65924ab5d99411394f187dde27abd84a22
신주훈 <junefinger@gmail.com>
2026-07-26 15:28:54 +0900

    feat: 제헌절 popup 제거

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const App = () => {
         <>
         <div>
           <PopupGroup
-            images={[imageurl2, imageurl8, imageurl7, imageurl1]}
+            images={[imageurl8, imageurl7, imageurl1]}
             width={360 * ratio}
             height={500 * ratio}
             gap={20}
@@ -87,25 +87,6 @@ const App = () => {
       </div>
       {isMobile && (
         <>
-          <div
-            style={{
-              width: "100%",
-              height: "100%",
-              border: "4px solid #7ecbff",   // ⭐ 하늘색 + 두꺼운 선
-              overflow: "hidden",
-              boxSizing: "border-box"
-            }}
-          >
-            <img
-              src={imageurl2}
-              alt=""
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "cover",
-              }}
-            />
-          </div>
           <div
             style={{
               width: "100%",
